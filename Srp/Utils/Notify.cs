44b428e9a1ed08d43d3373217418a87a764184d5
Valentim Helio de Oliveira <timvale@hotmail.com>
2023-11-01 16:37:20 -0400

    Adicionar arquivos de projeto.

---
+++ Srp/Utils/Notify.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Srp.Utils
+{
+    class Notify
+    {
+
+        public Notify(Client client)
+        {
+            Console.WriteLine("Notificando o Cliente");
+        }
+    }
+}
